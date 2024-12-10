4eab4f5db04d8708d656f3509b694a3c21ab4e5d
matthewdo2 <555758@my.cuhsd.org>
2024-12-10 11:57:20 -0800

    Changed a bit of the comments for gautam

--- src/cashier.java
+++ src/cashier.java
@@ -2,14 +2,16 @@ import java.util.*;
 public class cashier {
 
     public static void main(String[] args){
+        //This is main class, cashier. If it has to be titled main thats chill
+        // But having it named cashier is a nice touch
     Scanner in = new Scanner(System.in);
-    String input = "";
+    String input;
 
 
-    System.o`ut.println("Welcome to Jojo's! Would you like to order? (y or n)");
+    System.out.println("Welcome to Jojo's! Would you like to order? (y or n)");
     input = in.nextLine();
     while (!input.equalsIgnoreCase("n")){
-            System.out.println("What would you like to order? ")
+            System.out.println("What would you like to order? ");
                     //Takes order by item (askinge each question such as condiments? or
         // dessert? and just normal cashier upselling and loops until their order is done.
         // First question should ask combo 1, 2, or 3, and we should be able to interpret
@@ -20,6 +22,6 @@ public class cashier {
         // wait time based on items. Sleep for that amnt. of time (in seconds rather
         // than minutes and then give them their order
 
-    System.out.println("Thanks for coming! You have a good day now.")
+    System.out.println("Thanks for coming! You have a good day now.");
     }
 }
